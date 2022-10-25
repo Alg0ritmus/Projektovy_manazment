c7443bdff15f1e8fb3c5fd251364e00018d44358
unknown <P.zelenak1@gmail.com>
2022-10-25 07:44:51 +0200

    mapped DB + firebase JWT

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+
 ## add JWT imports
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#installation
 
@@ -16,4 +18,12 @@ urlpatterns = [
     # JWT urls
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('firebase-reg/',views.fb_registration_view,name="fb_registration_view"),
+    path('pure-firebase-reg/',views.pure_fb_registration_view,name="pure_fb_registration_view"),
+
+    
+    #path('firebase-login/',views.fb_login_view,name="fb_login_view"),
+    #path('firebase-logout/',views.fb_logout_view,name="fb_logout_view"),
+
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,13 @@
 ## Django imports
 from django.contrib.auth.models import User, Group
+import pyrebase
+
+
+import json
+
+import uuid
+
+
 
 ## DRF imports
 from rest_framework.decorators import api_view
@@ -26,3 +34,117 @@ def registration_view(request):
 
 
     return Response({"Success":"User is sucessfully registrated!"})
+
+
+###############################################################
+
+
+
+
+
+@api_view(['POST'])
+def fb_registration_view(request):
+    firebaseConfig = {
+    "apiKey": "AIzaSyAqYEbX3wP8cMWpp3LYrJnmlnoiUoAhzbc",
+    "authDomain": "st-project-62715.firebaseapp.com",
+    "projectId": "st-project-62715",
+    "storageBucket": "st-project-62715.appspot.com",
+    "messagingSenderId": "771921370520",
+    "appId": "1:771921370520:web:6fda1cd80a2785b7fe5fed",
+    "measurementId": "G-1G83VXG6Z5",
+    "databaseURL": ""
+    }
+    firebase=pyrebase.initialize_app(firebaseConfig)
+    authe = firebase.auth()
+    #database=firebase.database()    
+
+    """
+    [{
+        "pass":"skuska",
+        "email":"skuska@skuska.com"
+    }]
+    """
+
+    email=json.loads(request.body.decode())[0]["email"] or request.POST.get("email") 
+    pasw=json.loads(request.body.decode())[0]["pass"] or request.POST.get("pass") 
+
+    print(email,pasw)
+
+
+    try:
+        # if there is no error then signin the user with given email and password
+        user=authe.sign_in_with_email_and_password(email,pasw)
+    except:
+        message="Invalid Credentials!!Please ChecK your Data"
+        return Response({"message":message})
+
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+
+    return Response({"user":user['idToken'],"email":email,"user_info":user})
+
+
+
+
+"""
+def postsignUp(request):
+     email = request.POST.get('email')
+     passs = request.POST.get('pass')
+     name = request.POST.get('name')
+     try:
+        # creating a user with the given email and password
+        user=authe.create_user_with_email_and_password(email,passs)
+        uid = user['localId']
+        idtoken = request.session['uid']
+        print(uid)
+     except:
+        return render(request, "Registration.html")
+     return render(request,"Login.html")
+
+"""
+
+@api_view(['POST'])
+def pure_fb_registration_view(request):
+    pass
+
+"""
+
+# https://firebase.google.com/docs/auth/admin/create-custom-tokens#python
+
+    #import os
+    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="creds.json"
+
+    firebaseConfig = {
+    "apiKey": "AIzaSyAqYEbX3wP8cMWpp3LYrJnmlnoiUoAhzbc",
+    "authDomain": "st-project-62715.firebaseapp.com",
+    "projectId": "st-project-62715",
+    "storageBucket": "st-project-62715.appspot.com",
+    "messagingSenderId": "771921370520",
+    "appId": "1:771921370520:web:6fda1cd80a2785b7fe5fed",
+    "measurementId": "G-1G83VXG6Z5",
+    "databaseURL": ""
+    }
+    
+
+    options = {
+        'serviceAccountId': 'tPerULojkdSWNFVSJvQtnv9BcXh2@st-project-62715.iam.gserviceaccount.com',
+    }
+    
+   
+
+    fb=firebase_admin.initialize_app(options=firebaseConfig)
+    fb.get_credentials()
+    
+    
+    uid = str(uuid.uuid4())
+    additional_claims = {
+        "premiumAccount": True,
+        "name":"skuska"
+    }
+
+
+    
+    custom_token = auth.create_custom_token(uid, additional_claims,app=fb)
+     
+    return Response({"custom_token":"asc"})
+"""
